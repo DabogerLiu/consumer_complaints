c1f07b64172b27c684f1b45f4d9600e7b09b03e3
DabogerLiu <61633589+DabogerLiu@users.noreply.github.com>
2020-03-20 19:55:38 -0400

    change input

--- src/consumer_complaints.py
+++ src/consumer_complaints.py
@@ -1,6 +1,6 @@
 import csv
 
-with open('./input/consumer_complaints.csv', newline='') as csvfile:
+with open('./input/complaints.csv', newline='') as csvfile:
   reader = csv.DictReader(csvfile)
   data = next(reader)
   print(data)
