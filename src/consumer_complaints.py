6f0c908541996015440cc92f57c148efdb0cc40f
DabogerLiu <61633589+DabogerLiu@users.noreply.github.com>
2020-03-20 17:05:16 -0400

    main code

---
+++ src/consumer_complaints.py
@@ -0,0 +1,20 @@
+import numpy as np
+import pandas as pd
+
+data_dir =  "C:/Users/Daboger/PycharmProjects/Insight_interview/"
+complains = pd.read_csv(data_dir+'complaints.csv')
+
+complains['Date received'] =  pd.to_datetime(complains['Date received']).dt.to_period('Y')
+def max_percent(x):
+    return 100 * x.value_counts().max()/x.count()
+report = complains.groupby(['Product','Date received']).agg(
+    {
+
+        'Product':'size',
+        'Company': ['nunique',max_percent]
+    }
+)
+print(report)
+report.reset_index().to_csv (r'C:/Users/Daboger/PycharmProjects/Insight_interview/report.csv', index = False, header=False)
+
+
