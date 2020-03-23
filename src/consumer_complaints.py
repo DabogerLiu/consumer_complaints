554b50c2e3712558e718372031f5ab1a86e1b7f8
Shaobo Liu <61633589+DabogerLiu@users.noreply.github.com>
2020-03-23 16:17:02 -0400

    Update consumer_complaints.py

--- src/consumer_complaints.py
+++ src/consumer_complaints.py
@@ -1,4 +1,3 @@
-# Read CSV file
 import csv
 from collections import Counter
 
@@ -51,6 +50,41 @@ for i in range(len(report)):
   report[i]['Total_company']= len(Counter(ListsofCompany[i]).keys())
   report[i]['max_frequency_company'] = round(max(Counter(ListsofCompany[i]).values())*100/report[i]['Total_complains'])
 
+Product = list()
+Year = list()
+Total_complains = list()
+Total_company = list()
+max_frequency_company = list()
 for i in range(len(report)):
-  print(report[i])
+  Product.append(report[i]['Product'])
+  Year.append(report[i]['Year'])
+  Total_complains.append(report[i]['Total_complains'])
+  Total_company.append(report[i]['Total_company'])
+  max_frequency_company.append(report[i]['max_frequency_company'])
+#sorted_Product = sorted(Product)
+#print(sorted_Product)
+sorted_index = sorted(range(len(Product)),key=Product.__getitem__)  #get the index list
 
+sorted_Product = sorted(Product)
+
+sorted_Year = list()
+sorted_Total_complains = list()
+sorted_Total_company = list()
+sorted_max_frequency_company = list()
+
+for index in range(len(report)):
+  sorted_Year.append(Year[sorted_index[index]])
+  sorted_Total_complains.append(Total_complains[sorted_index[index]])
+  sorted_Total_company.append(Total_company[sorted_index[index]])
+  sorted_max_frequency_company.append(max_frequency_company[sorted_index[index]])
+
+for i in range(len(report)):
+  print(sorted_Product[i],sorted_Year[i], sorted_Total_complains[i],sorted_Total_company[i],sorted_max_frequency_company[i])
+
+with open('./output/report.csv', 'w', newline='') as csvfile:
+  fieldnames = ['Product', 'Year','Total_complains','Total_company','sorted_max_frequency_company']
+  writer = csv.DictWriter(csvfile,fieldnames=fieldnames)
+  for i in range(len(report)):
+    writer.writerow({'Product':sorted_Product[i], 'Year':sorted_Year[i],'Total_complains':sorted_Total_complains[i],'Total_company':sorted_Total_company[i],'sorted_max_frequency_company':sorted_max_frequency_company[i]})
+    #writer.writerow({sorted_Product[i],sorted_Year[i], sorted_Total_complains[i],sorted_Total_company[i],sorted_max_frequency_company[i]})
+  #writer.writeheader()
