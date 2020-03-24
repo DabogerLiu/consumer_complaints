66791f54a8417b3a62b88cfc442fe86afd3205fc
Shaobo Liu <61633589+DabogerLiu@users.noreply.github.com>
2020-03-24 19:18:08 -0400

    Update consumer_complaints.py

--- src/consumer_complaints.py
+++ src/consumer_complaints.py
@@ -15,48 +15,52 @@ def csv_dict_list(variables_file):
 complains = csv_dict_list('./input/consumer_complaints.csv')
 #complains = csv_dict_list('C:/Users/Daboger/Desktop/test_1/input/complaints.csv')
 key1 = str(complains[0]['Product'])
-key2 = str(complains[0]['Date received'][0:4])
+key2 = str(complains[0]['Date received'][-4:])
 key = list()
 key.append(key1 + key2)
 # print(key)
 
 
+key1 = str(complains[0]['Product']) 
+key2 = str(complains[0]['Date received'][-4:])
+key = list()
+key.append( key1 + key2)
+#print(key)
+
+
 for i in range(len(complains)):
-  key.append(str(complains[i]['Product']) + str(complains[i]['Date received'][0:4]))
+  key.append(str(complains[i]['Product'])+str(complains[i]['Date received'][-4:]))
 
 report = Counter(key).keys()
 sorted_report = sorted(report)
 product = list()
 year = list()
 
-for i in range(len(sorted_report)):
-  year.append(sorted_report[i][0:4])
-  product.append(sorted_report[i][4:])
 
+for i in range(len(sorted_report)):
+  year.append(sorted_report[i][-4:])
+  product.append(sorted_report[i][0:len(sorted_report[i])-4])
+  
 Total_complains = list()
 for i in range(len(report)):
   counter = 0
   for j in range(len(complains)):
-    if (product[i] == complains[j]['Product']) & (year[i] == complains[j]['Date received'][0:4]):
-      counter = counter + 1
+    if (product[i]==complains[j]['Product'])&(year[i]==complains[j]['Date received'][-4:]):
+      counter = counter+1
   Total_complains.append(counter)
 
 ListsofCompany = []
 for i in range(len(report)):
-  company = []
+  company=[]
   for j in range(len(complains)):
-    if (product[i] == complains[j]['Product']) & (year[i] == complains[j]['Date received'][0:4]):
+    if (product[i] == complains[j]['Product'])&(year[i] == complains[j]['Date received'][-4:]):
       company.append(complains[j]['Company'])
   ListsofCompany.append(company)
-  
 Total_company = list()
-
-for i in range(len(report)):
-  Total_company.append(len(Counter(ListsofCompany[i]).keys()))
-  
 max_frequency_company = list()
 for i in range(len(report)):
-  max_frequency_company.append(round(max(Counter(ListsofCompany[i]).values()) * 100 / Total_complains[i]))
+  Total_company.append(len(Counter(ListsofCompany[i]).keys()))
+  max_frequency_company.append(round(max(Counter(ListsofCompany[i]).values())*100/Total_complains[i]))
 
 with open('./output/report.csv', 'w', newline='') as csvfile:
   fieldnames = ['Product', 'Year','Total_complains','Total_company','max_frequency_company']
