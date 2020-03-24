c06466fa728cbcde9481180364e2b9fc33a1b06f
Shaobo Liu <61633589+DabogerLiu@users.noreply.github.com>
2020-03-24 08:52:45 -0400

    Update consumer_complaints.py

--- src/consumer_complaints.py
+++ src/consumer_complaints.py
@@ -12,7 +12,7 @@ def csv_dict_list(variables_file):
   return dict_list
 
 
-complains = csv_dict_list('  ./insight_testsuite/tests/test_2/input/consumer_complaints.csv')
+complains = csv_dict_list('  ../insight_testsuite/tests/test_2/input/consumer_complaints.csv')
 
 #complains = csv_dict_list('C:/Users/Daboger/PycharmProjects/Insight_interview/complaints.csv')
 report = {}
@@ -81,7 +81,7 @@ for index in range(len(report)):
 for i in range(len(report)):
   print(sorted_Product[i],sorted_Year[i], sorted_Total_complains[i],sorted_Total_company[i],sorted_max_frequency_company[i])
 
-with open(' ./insight_testsuite/tests/test_2/output/report.csv', 'w', newline='') as csvfile:
+with open(' ../insight_testsuite/tests/test_2/output/report.csv', 'w', newline='') as csvfile:
   fieldnames = ['Product', 'Year','Total_complains','Total_company','sorted_max_frequency_company']
   writer = csv.DictWriter(csvfile,fieldnames=fieldnames)
   for i in range(len(report)):
