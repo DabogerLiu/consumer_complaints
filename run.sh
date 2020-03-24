1f553a6c1315ae71fa4f8a0347e377f42039cf23
Shaobo Liu <61633589+DabogerLiu@users.noreply.github.com>
2020-03-24 08:38:56 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python3 ./src/consumer_complaints.py ./insight_testsuite/tests/test/test_1/input/consumer_complaints.csv ./insight_testsuite/tests/test_1/output/report.csv
+python3 ./src/consumer_complaints.py ./insight_testsuite/tests/test_1/input/consumer_complaints.csv ./insight_testsuite/tests/test_1/output/report.csv
